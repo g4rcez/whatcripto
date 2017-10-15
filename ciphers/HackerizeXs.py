c322b70a7e6fca1cfdde4dbab364eeb2ab80c0c5
vandalvnl <allan.f.garcez@gmail.com>
2017-10-15 19:29:07 -0200

    Fix for printable ciphers

--- ciphers/HackerizeXs.py
+++ ciphers/HackerizeXs.py
@@ -1,23 +1,21 @@
 class HackerizeXs:
     def __init__(self, cipher):
         self.simbols = {
-            '₪': 'w', '¶': 'q', '☰': 'e', '┏': 'r',
+            'Λ': 'a','₪': 'w', '¶': 'q', '☰': 'e', '┏': 'r',
             '⊥': 't', '¥': 'y', 'ü': 'u', '¡': 'i',
-            '☐': 'o', 'þ': 'p', 'Λ': 'a', '§': 's',
+            '☐': 'o', 'þ': 'p', '§': 's',
             'Ð': 'd', '∲': 'f', 'ç': 'g', 'ƴ': 'v',
             'ß': 'b', '∏': 'n', 'ღ': 'm', '╫': 'h',
             '¿': 'j', '├': 'k', '↑': 'l', 'ᶾ': 'z',
-            '✕': 'x', '↻': 'c',
+            '✕': 'x', '↻': 'c', ' ': ' ', '{':'{',
+            '}':'}','[':'[',']':']',"!":"!","?":"?","▪":'.'
         }
         self.cipher = cipher
-    
     def decipher(self):
         decipherList = []
         for char in self.cipher:
             try:
-                decipherList.append(
-                    self.simbols[char.lower()]
-                )
-            except KeyError:
-                decipherList.append(char)
+                decipherList.append(self.simbols.get(char))
+            except:
+                pass
         return ''.join(decipherList)
