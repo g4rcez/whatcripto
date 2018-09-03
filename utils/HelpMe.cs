04f78b830c084f8bbd3dd063082e098034673076
Garcez <allan.f.garcez@gmail.com>
2018-09-03 10:27:55 -0300

    Write help menu

--- utils/HelpMe.cs
+++ utils/HelpMe.cs
@@ -3,11 +3,17 @@ namespace whatcripto.utils {
         public static string maintainer() {
             return @"Whatcripto v3.0.0 - Search and Destroy for ciphers
 GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
-            
+
 Writen by Allan 'vandalvnl' Garcez
 Homepage: https://github.com/vandalvnl/whatcripto
 Issues:https://github.com/vandalvnl/whatcripto/issues
 or mail-me: vandal.vnl.dev@gmail.com";
+        }
+
+        public static string helpOptions(){
+            return @"-h, --help: Open this menu
+            -k, --key: Pass a key to cipher keys
+            ";
         }
     }
 }
